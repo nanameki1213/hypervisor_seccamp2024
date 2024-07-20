4b4af766cc844e719f02f66fb33950714d636690
nanameki1213 <nanamekitaishin1213@icloud.com>
2024-07-21 08:51:25 +0900

    virt_mmioのversionとdevice idの読み取りに対する応答を追加

--- src/mmio/virt_mmio.rs
+++ src/mmio/virt_mmio.rs
@@ -1,12 +1,35 @@
+//
+// virt mmio 仮想デバイス
+//
+
 const VIRT_MMIO: usize = 0xa000000;
 const VIRT_MMIO_SIZE: usize = 0x200;
 
-pub fn virt_mmio_read(offset: usize, _access_width: u64) -> Result<u64, ()>
+const VIRT_MMIO_MAGIC_VALUE: u32 = 0x74726976;
+const VIRT_MMIO_VERSION: u32 = 0x2;
+
+const VIRTIO_RESERVED: u32 = 0x00;
+const VIRTIO_NETWORK_CARD: u32 = 0x01;
+const VIRTIO_BLOCK_DEVICE: u32 = 0x02;
+
+const VIRT_MMIO_MAGIC_OFFSET: usize = 0x00;
+const VIRT_MMIO_VERSION_OFFSET: usize = 0x04;
+const VIRT_MMIO_DEVICE_ID_OFFSET: usize = 0x08;
+const VIRT_MMIO_VENDOR_ID_OFFSET: usize = 0x0c;
+
+pub fn virt_mmio_read(device_type: u32, offset: usize, _access_width: u64) -> Result<u32, ()>
 {
-    
+    match offset {
+        VIRT_MMIO_MAGIC_OFFSET => Ok(VIRT_MMIO_MAGIC_VALUE),
+        VIRT_MMIO_VERSION_OFFSET => Ok(0x2),
+        VIRT_MMIO_DEVICE_ID_OFFSET => Ok(device_type),
+        _ => {
+            Err(())
+        }
+    }
 }
 
-pub fn virt_mmio_write(offset: usize, _access_width: u64, value: u64) -> Result<(), ()>
+pub fn virt_mmio_write(device_type: u32, offset: usize, _access_width: u64, value: u32) -> Result<(), ()>
 {
-
+    
 }
